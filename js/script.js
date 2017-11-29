e2dcdef4d0996285ad821b2ed951a050e3306531
Samuel Soares <samuelsoares179@gmail.com>
2017-11-29 15:06:30 -0300

    Adicionando codigo javascript que realiza calculo de soma ao click do botão

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
- 
+
+function somar(a,b){
+	return parseInt(a) + parseInt(b);
+}
+
+
+document.querySelector("#calcular").addEventListener("click",function(){
+ 	let valorA = document.querySelector("#valorA").value;
+ 	let valorB = document.querySelector("#valorB").value;
+
+ 	if (valorA.length >0 && valorB.length >0){
+ 		
+ 		//alert(parseInt(valorA)+parseInt(valorB));	
+ 		alert(somar(valorA,valorB));
+
+ 	} else {
+ 		
+ 		alert("Digite os valores a serem somados");
+
+ 	}
+ 	
+ })
